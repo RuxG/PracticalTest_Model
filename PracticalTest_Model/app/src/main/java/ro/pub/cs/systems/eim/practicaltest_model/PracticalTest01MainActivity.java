4d6df1dea274b0e6d9a1aac9ee04ddd9ab64e8b3
Ruxandra Grigorie <ruxi.grigorie@gmail.com>
2022-04-10 12:23:14 +0300

    B1

--- PracticalTest_Model/app/src/main/java/ro/pub/cs/systems/eim/practicaltest_model/PracticalTest01MainActivity.java
+++ PracticalTest_Model/app/src/main/java/ro/pub/cs/systems/eim/practicaltest_model/PracticalTest01MainActivity.java
@@ -2,12 +2,48 @@ package ro.pub.cs.systems.eim.practicaltest_model;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 
 public class PracticalTest01MainActivity extends AppCompatActivity {
+
+    EditText zeroText;
+    EditText oneText;
+    Button zeroButton;
+    Button oneButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practical_test_model_main);
+
+        zeroText = (EditText)findViewById(R.id.text_view_0);
+        oneText = (EditText)findViewById(R.id.text_view_1);
+        zeroButton = (Button)findViewById(R.id.button_0);
+        oneButton = (Button)findViewById(R.id.button_1);
+
+        PressMeListener listener = new PressMeListener();
+        zeroButton.setOnClickListener(listener);
+        oneButton.setOnClickListener(listener);
     }
+
+    private class PressMeListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+
+            if (v.getId() == R.id.button_0) {
+                int pressedNo = Integer.parseInt(zeroText.getText().toString());
+                pressedNo++;
+                zeroText.setText(String.valueOf(pressedNo));
+            } else if (v.getId() == R.id.button_1) {
+                int pressedNo = Integer.parseInt(oneText.getText().toString());
+                pressedNo++;
+                oneText.setText(String.valueOf(pressedNo));
+            }
+        }
+    }
+
+
 }
